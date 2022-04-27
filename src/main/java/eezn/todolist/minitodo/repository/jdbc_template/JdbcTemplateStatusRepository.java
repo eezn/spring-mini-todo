10486834fe1b1f3d3a7d08277f0d49b85331ab2e
jin-lee <pplaneoo@gmail.com>
2022-04-27 21:19:06 +0900

    feat(repository): add status repository, test code (#7)

---
+++ src/main/java/eezn/todolist/minitodo/repository/jdbc_template/JdbcTemplateStatusRepository.java
@@ -0,0 +1,58 @@
+package eezn.todolist.minitodo.repository.jdbc_template;
+
+import eezn.todolist.minitodo.domain.Status;
+import eezn.todolist.minitodo.repository.StatusRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class JdbcTemplateStatusRepository implements StatusRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    /** 현재 사용하지 않음 */
+    @Override
+    public Status insert(Status status) {
+
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("todo_status").usingGeneratedKeyColumns("status_id");
+        SqlParameterSource params = new MapSqlParameterSource()
+                .addValue("status_name", status.getStatus())
+                .addValue("status_is_deleted", status.getIsDeleted());
+
+        int key = jdbcInsert.executeAndReturnKey(params).intValue();
+        status.setId(key);
+        return status;
+    }
+
+    @Override
+    public Optional<Status> findById(Integer id) {
+        List<Status> result = jdbcTemplate.query(
+                "select * from todo_status where status_id = ?", statusRowMapper(), id);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Status> findAll() {
+        return jdbcTemplate.query("select * from todo_status", statusRowMapper());
+    }
+
+    private RowMapper<Status> statusRowMapper() {
+        return (rs, rowNum) -> {
+            Status status = new Status();
+            status.setId(rs.getInt("status_id"));
+            status.setStatus(rs.getString("status_name"));
+            status.setIsDeleted(rs.getBoolean("status_is_deleted"));
+            return status;
+        };
+    }
+}
---
+++ src/test/java/eezn/todolist/minitodo/repository/JdbcTemplateStatusTest.java
@@ -0,0 +1,44 @@
+package eezn.todolist.minitodo.repository;
+
+import eezn.todolist.minitodo.domain.Status;
+import eezn.todolist.minitodo.repository.jdbc_template.JdbcTemplateStatusRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class JdbcTemplateStatusTest {
+
+    @Autowired
+    JdbcTemplateStatusRepository repository;
+
+    @Test
+    public void findByIdTest() {
+
+        Status statusDone = repository.findById(1).get();
+        Status statusTodo = repository.findById(2).get();
+
+        System.out.println(statusDone);
+        System.out.println(statusTodo);
+
+        assertThat("done").isEqualTo(statusDone.getStatus());
+        assertThat("todo").isEqualTo(statusTodo.getStatus());
+    }
+
+    @Test
+    public void findAllTest() {
+
+        List<Status> findAll = repository.findAll();
+
+        for (Status status : findAll) {
+            System.out.println(status.toString());
+        }
+
+        assertThat("done").isEqualTo(findAll.get(0).getStatus());
+        assertThat("todo").isEqualTo(findAll.get(1).getStatus());
+    }
+}
