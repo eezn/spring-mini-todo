0dced5fce56c542c0804479bc25ba0042b4fa32f
jin-lee <pplaneoo@gmail.com>
2022-04-27 21:08:31 +0900

    feat(domain): Add domains (#4)

---
+++ src/main/java/eezn/todolist/minitodo/domain/User.java
@@ -0,0 +1,26 @@
+package eezn.todolist.minitodo.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *  `user_id`           INT             NOT NULL AUTO_INCREMENT,
+ * 	`user_name`	        VARCHAR(255)    NULL,
+ * 	`user_password`	    VARCHAR(255)    NULL,
+ * 	`user_email`	    VARCHAR(255)    NULL,
+ * 	`user_is_deleted`	BOOLEAN         NULL    DEFAULT FALSE
+ */
+
+@Getter
+@Setter
+@ToString
+public class User {
+
+    private Integer id;
+
+    private String username;
+    private String password;
+    private String email;
+    private Boolean isDeleted;
+}
