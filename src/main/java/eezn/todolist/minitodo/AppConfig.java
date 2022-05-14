752c876fafa8bc05308c8fc2502b3d232ecb2ae3
jin-lee <pplaneoo@gmail.com>
2022-05-14 19:47:47 +0900

    fix(AppConfig): Remove duplicated bean creation

--- src/main/java/eezn/todolist/minitodo/AppConfig.java
+++ src/main/java/eezn/todolist/minitodo/AppConfig.java
@@ -8,8 +8,6 @@ import eezn.todolist.minitodo.repository.jdbctemplate.JdbcTemplateCategoryReposi
 import eezn.todolist.minitodo.repository.jdbctemplate.JdbcTemplatePriorityRepository;
 import eezn.todolist.minitodo.repository.jdbctemplate.JdbcTemplateTodoRepository;
 import eezn.todolist.minitodo.repository.jdbctemplate.JdbcTemplateUserRepository;
-import eezn.todolist.minitodo.service.TodoService;
-import eezn.todolist.minitodo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,15 +19,15 @@ public class AppConfig {
 
     public final JdbcTemplate jdbcTemplate;
 
-    @Bean
-    public UserService userService() {
-        return new UserService(userRepository(), todoRepository());
-    }
-
-    @Bean
-    public TodoService todoService() {
-        return new TodoService(userRepository(), todoRepository(), categoryRepository(), priorityRepository());
-    }
+//    @Bean
+//    public UserService userService() {
+//        return new UserService(userRepository(), todoRepository());
+//    }
+//
+//    @Bean
+//    public TodoService todoService() {
+//        return new TodoService(userRepository(), todoRepository(), categoryRepository(), priorityRepository());
+//    }
 
     @Bean
     public UserRepository userRepository() {
